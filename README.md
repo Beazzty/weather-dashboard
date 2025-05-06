23b7aeca5e40411eaef5c6bbf8f037f9ad223ff6
Quinn Brown <184797872+Beazzty@users.noreply.github.com>
2025-05-06 10:35:53 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# takeout-menu
-# weather_app
+# weather app
+- simple weather app that shows the weather
+
+- Table of Contents
+
+- [installation](##installation)
+- [Usage](##Usage)
+- [credits](##credits)
+- [contact](##contact)
+- [Link](##Link)
+
+# Installation
+- Instructions on how to install project
+- git clone 
+
+- cd bootcamp/Challange-9
+- npm install
+
+# Usage
+- ```bash
+- Run the project
+- npm start
+# Credits
+- Chat GPT
+- Xpert learning
+
+# Contributing
+- Guidelines for contributing to the project.
+
+- Fork the repository.
+- Create a new branch (git checkout -b feature-branch).
+- Make your changes.
+- Commit your changes (git commit -m 'Add some feature').
+- Push to the branch (git push origin feature-branch).
+- Open a pull request.
+
+# Contact
+- Quinn Brown - quinnbrown2004@gmail.com
+
+# Link
+- https://github.com/Beazzty/Vehicle-builder
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,30 +2,58 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
 
 // TODO: Define a class for the Weather object
+interface Weather {
+  date: string;
+  temperature: number;
+  description: string;
+  icon: string;
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
+city: string;
+baseURL: string;
+apiKey: string;
+
+
   // TODO: Define the baseURL, API key, and city name properties
-  // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
-  // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
-  // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
-  // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
-  // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
-  // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
-  // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  constructor() {
+    this.city = '';
+    this.baseURL = process.env.WEATHER_API_BASE_URL || '';
+    this.apiKey = process.env.WEATHER_API_KEY || '';
+  }
+
+//TODO:fetch latitude and the longitude the user searched for first
+async Coordinates(city: string){
+  const url = `${this.baseURL}/geo/1.0/direct?q=${city}&limit=1&appid=${this.apiKey}`;
+}
+
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+//TODO:fetch the weather from the longitude and the latitude from the previious TODO
+async Weather(coordinates: Coordinates){
+  const url = `${this.baseURL}/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.apiKey}`;
+}
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//TODO: once we get the weather we need to format it and send it back to the client
+async formatWeather(data: any): Promise<Weather[]> {
+  const weatherList: Weather[] = data.list.map((item: any) => ({
+    date: item.dt_txt,
+    temperature: item.main.temp,
+    description: item.weather[0].description,
+    icon: item.weather[0].icon,
+  }));
+  return weatherList;
+  
+}
 }
 
 export default new WeatherService();
